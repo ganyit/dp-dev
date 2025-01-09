14d3e6d695d689bda268a07e9d20bcb85a6cab53
Prem Ganesh <ganyit@gmail.com>
2025-01-09 12:48:38 +0000

    addtional changes

--- web/modules/custom/task_manager/src/Controller/TaskManagerController.php
+++ web/modules/custom/task_manager/src/Controller/TaskManagerController.php
@@ -20,8 +20,15 @@ class TaskManagerController extends ControllerBase {
       //print_r($student);exit;
       $studentsPaymentList[$i]['studentname'] = $field_student_name = $student->get('field_student_name')->value;
       $class_id = $student->get('field_class')->target_id;
-      $studentsPaymentList[$i]['classname'] = $this->loadClassDetail($class_id);
-      $studentsPaymentList[$i]['term1'] = $this->loadStudentPayment($student->id());
+      $classDetails = $this->loadClassDetail($class_id);
+      $studentsPaymentList[$i]['classname'] = $classDetails['name'];
+      $studentsPaymentList[$i]['totalFees'] = $classDetails['fees'];
+      $termPayments = $this->loadStudentPayment($student->id());
+      $studentsPaymentList[$i]['term1'] = $termPayments['term1'];
+      $studentsPaymentList[$i]['term2'] = $termPayments['term2'];
+      $totalPaid = $termPayments['term1'] + $termPayments['term2'];
+      $studentsPaymentList[$i]['balanceFee'] = $studentsPaymentList[$i]['totalFees'] - $totalPaid;
+      $i++;
       #echo "student Id---".$student->id()."====";          
       #print_r($field_student_name.$class_id);
     }
@@ -40,11 +47,11 @@ class TaskManagerController extends ControllerBase {
       ->execute();
 
     $classes = $schoolClasses->loadMultiple($ids);
-    foreach ($classes as $class) {
-      #echo "---from loadClassDetail";
-      #print_r($class->get('field_class_name')->value);
+    foreach ($classes as $class) {    
+      $classDetails['name'] = $class->get('field_class_name')->value;
+      $classDetails['fees'] = $class->get('field_fees')->value;
     }
-    return $class->get('field_class_name')->value;
+    return $classDetails;
   }
 
   public function loadStudentPayment($student_id) {
@@ -57,9 +64,11 @@ class TaskManagerController extends ControllerBase {
     $payments = $schoolPayments->loadMultiple($ids);
     foreach ($payments as $payment) {
       #echo "from paymentDetail";
-      #print_r($payment->get('field_term_1')->value);
+      #print_r($payment->get('field_term_1')->value);    
+      $termPayments['term1'] = $payment->get('field_term_1')->value;
+      $termPayments['term2'] = $payment->get('field_term_2')->value;
     }
-    return $payment->get('field_term_1')->value;
+    return $termPayments;
   }
 
 }
